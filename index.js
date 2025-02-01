32979e1201d3e556acf33a248581e5c28951ae68
bhanudevc <bhanudevchauhan@gmail.com>
2025-02-01 11:47:37 +0530

    this is for the merging the both commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let digit = document.querySelector(".num").textContent;
+console.log(Number(digit));
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+  console.log("button clicked");
+  digit = Number(digit) + 1;
+  document.querySelector(".num").textContent = digit;
+});
+
+const h_1 = document.querySelector(".h1");
+let isBoring = true;
+document.querySelector(".btn").addEventListener("click", function () {
+  if (isBoring) {
+    h_1.innerHTML = "This is very exciting day😊";
+  } else {
+    h_1.innerHTML = "This is very boring day😒";
+  }
+  isBoring = !isBoring;
+});
